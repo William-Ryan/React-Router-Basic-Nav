87573c9845fe2e6876fc3129e35f5715c430d77f
William Ryan <billy.ryan.rom@gmail.com>
2020-01-28 01:33:38 -0500

    Add Router to index.js/Add Route to App.js/Add Links to Navigation.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {
+  Link,
+  Switch,
+  Route
+} from "react-router-dom";
+
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
-const App = () => (
+const App = () => {
+  return(
   <div>
     <Navigation />
+    <Switch>
+      <Route exact path ="/" component={Home}/>
+      <Route path = "/about" component={About}/>
+      <Route path = "/contact" component={Contact}/>
+    </Switch>
   </div>
-);
+  )
+};
 
 export default App;
